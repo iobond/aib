fefcea6a310464f2a12102bd9ac05760305e0ef2
jun.wang <jun.wang@bitontop.com>
2018-10-30 18:07:39 +0100

    Fixed conf dir to .aibw

--- src/util.cpp
+++ src/util.cpp
@@ -81,8 +81,8 @@
 // Application startup time (used for uptime calculation)
 const int64_t nStartupTime = GetTime();
 
-const char * const BITCOIN_CONF_FILENAME = "bitcoin.conf";
-const char * const BITCOIN_PID_FILENAME = "bitcoind.pid";
+const char * const BITCOIN_CONF_FILENAME = "aib.conf";
+const char * const BITCOIN_PID_FILENAME = "aib.pid";
 
 ArgsManager gArgs;
 
@@ -704,13 +704,13 @@ void PrintExceptionContinue(const std::exception* pex, const char* pszThread)
 
 fs::path GetDefaultDataDir()
 {
-    // Windows < Vista: C:\Documents and Settings\Username\Application Data\Bitcoin
-    // Windows >= Vista: C:\Users\Username\AppData\Roaming\Bitcoin
-    // Mac: ~/Library/Application Support/Bitcoin
-    // Unix: ~/.bitcoin
+    // Windows < Vista: C:\Documents and Settings\Username\Application Data\Aib
+    // Windows >= Vista: C:\Users\Username\AppData\Roaming\Aib
+    // Mac: ~/Library/Application Support/Aib
+    // Unix: ~/.aib
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "Bitcoin";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "Aib";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -720,10 +720,10 @@ fs::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
     // Mac
-    return pathRet / "Library/Application Support/Bitcoin";
+    return pathRet / "Library/Application Support/Aib";
 #else
     // Unix
-    return pathRet / ".bitcoin";
+    return pathRet / ".aib";
 #endif
 #endif
 }
@@ -1252,3 +1252,4 @@ int ScheduleBatchPriority(void)
     return 1;
 #endif
 }
+
