7fbc1cab0c159d977045dec94cc06491d0933137
Your Name <temple@9cat.net>
2019-01-24 13:45:46 -0800

    add build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
+#!/bin/sh
+# Copyright (c) 2014-2019 The AIB Community developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+  
+    
     ./autogen.sh && \
     ./configure && \
     make -j8
