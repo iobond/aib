998ed5148ff9e35e0fb10b37f5dde0a72d6701e0
botchun <chunlee@bitontop.com>
2020-06-08 17:46:52 -0700

    modified depends files

--- build-linux.sh
+++ build-linux.sh
@@ -4,12 +4,12 @@
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 
-git checkout master
-git reset --hard HEAD
-git pull
-cd depends/
-make -j8
-cd ../
+#git checkout master
+#git reset --hard HEAD
+#git pull
+#cd depends/
+#make -j8
+#cd ../
 ./autogen.sh
 ./configure --enable-glibc-back-compat --prefix=`pwd`/depends/x86_64-pc-linux-gnu LDFLAGS="-static-libstdc++"   --disable-tests  --disable-bench
 make -j8
